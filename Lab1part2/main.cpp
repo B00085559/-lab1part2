a1f4823298b0cea9a798a71db99789533eb4528f
Joseph McGrath <B00085559@student.itb.ie>
2017-09-21 13:54:52 +0100

    Added the area of circle

--- Lab1part2/main.cpp
+++ Lab1part2/main.cpp
@@ -1,39 +1,53 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 // Base class
 class Shape
 {
 public:
- // pure virtual function providing interface framework.
- virtual int getArea() = 0;
- void setWidth(int w)
- {
- width = w;
- }
- void setHeight(int h)
- {
- height = h;
- }
+// pure virtual function providing interface framework.
+    virtual int getArea() = 0;
+    void setWidth(int w)
+    {
+        width = w;
+    }
+    void setHeight(int h)
+    {
+        height = h;
+    }
 protected:
- int width;
- int height;
+    int width;
+    int height;
 };
 class Triangle: public Shape
 {
 public:
- int getArea()
- {
- return (width * height)/2;
- }
+    int getArea()
+    {
+        return (width * height)/2;
+    }
 };
+class Circle: public Shape
+{
+public:
+    getArea()
+    {
+        return (3.1415 * pow(width/2,2));
+    }
 
+};
 
 int main(void)
 {
- Triangle Tri;
-  Tri.setWidth(5);
- Tri.setHeight(7);
- // Print the area of the object.
- cout << "Total Triangle area: " << Tri.getArea() << endl;
- return 0;
+    Triangle Tri;
+    Circle cir;
+
+    cir.setWidth(5);
+
+    Tri.setWidth(5);
+    Tri.setHeight(7);
+// Print the area of the object.
+    cout << "Total Triangle area: " << Tri.getArea() << endl;
+    cout << "Area of Circle: " << cir.getArea() << endl;
+    return 0;
 }
